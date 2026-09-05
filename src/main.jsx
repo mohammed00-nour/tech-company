0ae6819d6bc3750f5eee938fb278907d3b05b24b
mohammed <zazzazaa00@gmail.com    git config --global user.name Mohammed>
2026-09-05 14:21:19 +0300

    Fix React Router basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/tech-company">
+    
       <HelmetProvider>
         <App />
       </HelmetProvider>
